80385a319d341fc251783a2289c9a9e5955007df
Melchi M. Michel <melchimm@gmail.com>
2023-02-02 22:45:04 -0500

    added preprocessor macros for conditional declspec directives when compiled on Windows (using MSVC)
    - also added wheel distribution file for Python v3.10

--- pyrtools/pyramids/c/internal_pointOp.h
+++ pyrtools/pyramids/c/internal_pointOp.h
@@ -1,3 +1,9 @@
-void internal_pointop(double *im, double *res, int size, double *lut, 
+#ifdef _WIN32
+#define LIBRARY_API __declspec(dllexport)
+#else
+#define LIBRARY_API 
+#endif
+
+LIBRARY_API  void internal_pointop(double *im, double *res, int size, double *lut, 
 		      int lutsize, double origin, double increment, 
 		      int warnings);
